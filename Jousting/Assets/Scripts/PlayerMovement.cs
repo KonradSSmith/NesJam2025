ce24f8bd0ead491db18890b17d4eab5d54b62257
Konrad Sieniatecki-Smith <konradsmith99@gmail.com>
2025-06-03 13:51:02 -0400

    made steering inverted when reversing

--- Jousting/Assets/Scripts/PlayerMovement.cs
+++ Jousting/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private Vector2 moveInput;
 
+    bool reversing = false;
 
     private void Awake()
     {
@@ -30,12 +31,33 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //get wasd vector
         moveInput = actions.Player.Move.ReadValue<Vector2>();
 
+        //add forward/backward velocity
         rb.AddForce((transform.forward * moveInput.y) * acceleration);
         rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, speed);
 
-        transform.Rotate(0, moveInput.x * minCameraRotationAmount, 0);
+        //check if reversing
+        if (moveInput.y > 0)
+        {
+            reversing = false;
+        }
+        else if (moveInput.y < 0)
+        { 
+            reversing = true;
+        }
+
+        //invert steering if reversing
+        switch (reversing)
+        {
+            case false:
+                transform.Rotate(0, moveInput.x * minCameraRotationAmount, 0);
+                break;
+            case true:
+                transform.Rotate(0, -moveInput.x * minCameraRotationAmount, 0);
+                break;
+        }
     }
 
     // Update is called once per frame
