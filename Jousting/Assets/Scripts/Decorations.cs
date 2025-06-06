7ce5dbf4ffc37bd2a19146f2a4da6d8ea570c76c
Konrad Sieniatecki-Smith <konradsmith99@gmail.com>
2025-06-05 20:26:46 -0400

    first track pieces

--- Jousting/Assets/Scripts/Decorations.cs
+++ Jousting/Assets/Scripts/Decorations.cs
@@ -4,11 +4,13 @@ public class Decorations : MonoBehaviour
 {
     private Transform cameraTransform;
     private float scaleAmount;
+    private Vector3 defaultScale;
 
     // Use this for initialization
     void Start()
     {
         cameraTransform = Camera.main.transform;
+        defaultScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -16,6 +18,6 @@ public class Decorations : MonoBehaviour
     {
         scaleAmount = (-Vector3.Distance(transform.position, cameraTransform.position) / 40) + 1.5f;
         scaleAmount = Mathf.Clamp01(scaleAmount);
-        transform.localScale = new Vector3 (scaleAmount, scaleAmount, scaleAmount);
+        transform.localScale = defaultScale * scaleAmount;
     }
 }
